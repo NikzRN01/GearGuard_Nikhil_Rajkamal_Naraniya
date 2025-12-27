a011fa79df37583abd3a6da5dcd7f0ebdc973b5a
Tirth Jain <jaintirth2705@gmail.com>
2025-12-27 13:09:46 +0530

    Add forget password route and update the package

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const equipmentRoutes = require('./routes/equipmentRoutes');
 const teamRoutes = require('./routes/teamRoutes');
